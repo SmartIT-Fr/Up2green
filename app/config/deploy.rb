f692552cd21a13f59a0f4cc888cb6c26379586b8
Clement Gautier <clement.gautier.76@gmail.com>
2013-04-11 19:25:26 +0200

    Fixed deployment configuration

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,3 +1,9 @@
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::IMPORTANT
+# logger.level = Logger::INFO
+# logger.level = Logger::DEBUG
+# logger.level = Logger::MAX_LEVEL
+
 set :application, "www.up2green"
 set :domain,      "#{application}.com"
 set :deploy_to,   "/srv/www/#{domain}"
@@ -7,34 +13,31 @@ set :user,          "clement"
 set :repository,    "https://github.com/SmartIT-Fr/Up2green.git"
 set :scm,           :git
 set :model_manager, "propel"
-# set :deploy_via, :rsync_with_remote_cache
 set :deploy_via, :remote_cache
 set :dump_assetic_assets, true
 
 set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,   [app_path + "/logs", web_path + "/uploads"]
 set :use_composer,      true
-set :composer_options,  "--verbose --prefer-dist"
 
 set :writable_dirs,       ["app/cache", "app/logs", "web/uploads"]
 set :webserver_user,      "www-data"
 set :permission_method,   :acl
 set :use_set_permissions, true
+set  :keep_releases,  3
+set  :use_sudo,  false
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
-set  :keep_releases,  3
+# Building ACL classes
+after "symfony:propel:build:model", "symfony:propel:build:acl"
 
-# We need sudo for setfacl
-set  :use_sudo,  false
-# default_run_options[:pty] = true
+# Creating symlink for twitter bootstrap
+before "symfony:assetic:dump" do
+  run "#{try_sudo} sh -c 'cd #{latest_release} && #{php_bin} #{symfony_console} mopa:bootstrap:symlink:less --env=#{symfony_env_prod}'"
+end
 
-# Be more verbose by uncommenting the following line
-# logger.level = Logger::IMPORTANT
-logger.level = Logger::INFO
-# logger.level = Logger::DEBUG
-# logger.level = Logger::MAX_LEVEL
-
+after "deploy", "symfony:cache:clear"
 after "deploy", "deploy:cleanup"
