07e260b6bad61c22de3c0ad4c9b70afca685a7b6
Clément Gautier <clement.gautier.76@gmail.com>
2013-09-28 22:14:25 +0200

    Added a preview of the picture

--- src/Up2green/EducationBundle/Admin/ClassroomPictureAdmin.php
+++ src/Up2green/EducationBundle/Admin/ClassroomPictureAdmin.php
@@ -6,6 +6,7 @@ use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Up2green\EducationBundle\Model\ClassroomPicture;
 
 /**
  * Classroom picture admin class
@@ -17,19 +18,30 @@ class ClassroomPictureAdmin extends Admin
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
+        $uploadedFileOptions = array(
+            'label'    => 'form.classroom_picture_type.picture',
+            'required' => false,
+        );
+
+        /** @var ClassroomPicture $subject */
+        if (($subject = $this->getSubject()) && $subject->getPicture()) {
+            $path                               = $subject->getPicture();
+            $uploadedFileOptions['help_inline'] = '<img style="max-width:200px; max-height: 200px;" src="' . $path . '" />';
+        }
+
         $formMapper
             ->add('student', null, array(
                 'label' => 'form.classroom_picture_type.student'
             ))
             ->add('program', 'model', array(
-                'class'    => 'Up2green\ReforestationBundle\Model\Program',
-                'label'    => 'form.classroom_picture_type.program'
+                'class' => 'Up2green\ReforestationBundle\Model\Program',
+                'label' => 'form.classroom_picture_type.program',
             ))
             ->add('classroom', 'model', array(
-                'class'    => 'Up2green\EducationBundle\Model\Classroom',
-                'label'    => 'form.classroom_picture_type.program'
+                'class' => 'Up2green\EducationBundle\Model\Classroom',
+                'label' => 'form.classroom_picture_type.program'
             ))
-        ;
+            ->add('uploadedFile', 'file', $uploadedFileOptions);
     }
 
     /**
@@ -45,8 +57,7 @@ class ClassroomPictureAdmin extends Admin
                 'class' => 'Up2green\EducationBundle\Model\School',
             ))
             ->add('classroom')
-            ->add('classroom.year')
-        ;
+            ->add('classroom.year');
     }
 
     /**
@@ -60,7 +71,6 @@ class ClassroomPictureAdmin extends Admin
             ->add('program')
             ->add('classroom.school')
             ->add('classroom')
-            ->add('classroom.year')
-        ;
+            ->add('classroom.year');
     }
 }
